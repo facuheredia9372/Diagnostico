1a322150990c6e197f6724c26a01f3d1e7d208bb
¨Facundo <you@example.com>
2026-08-17 16:37:09 -0300

    otra forma

--- diagnostico.js
+++ diagnostico.js
@@ -1,3 +1,3 @@
-console.log('Versión de Node:', process.version);
+console.log('Versión de Node:', process.version)
 console.log('Versión de NPM:', require('child_process').execSync('npm -v').toString().trim());
 console.log('Versión de Git:', require('child_process').execSync('git --version').toString().trim());
